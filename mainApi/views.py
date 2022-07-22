7ddab123f1d9e4b4205d2983f3a22d6b39e5d64c
waynemorphic <wayneklyde@gmail.com>
2022-07-22 19:22:31 +0300

    added status messages to responses of class based apiviews

--- mainApi/views.py
+++ mainApi/views.py
@@ -1,4 +1,3 @@
-import imp
 from django.shortcuts import render
 from rest_framework.response import Response
 from django.http import JsonResponse, HttpResponse
@@ -8,7 +7,7 @@ from rest_framework.decorators import api_view
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.views import APIView
 from rest_framework.parsers import JSONParser, parse
-
+from rest_framework import status
 # Create your views here.
 
 class ArticlesData(APIView):
@@ -17,20 +16,19 @@ class ArticlesData(APIView):
         # serializer queryset
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many = True)
-        return Response(serializer.data)
+        return Response(serializer.data, status = status.HTTP_200_OK)
     
     # POST method    
     def post(self, request, format = None):     
         serializer = ArticleSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
             
             # if not valid
-        return Response(serializer.errors)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
-class ArticleDetail(APIView):
-        
+class ArticleDetail(APIView):        
     # get a single article depending on their primary key   
     def get(self, request, pk):
         try:
@@ -39,7 +37,7 @@ class ArticleDetail(APIView):
             return Response(serializer.data)
         
         except Article.DoesNotExist:
-            return Response(status = 404)
+            return Response(status = status.HTTP_404_NOT_FOUND)
         
     # alter the details of an article
     def put(self, request, pk):
@@ -48,13 +46,13 @@ class ArticleDetail(APIView):
         
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status = status.HTTP_205_RESET_CONTENT)
         
     # delete an article
     def delete(self, request, pk):
         articles = Article.objects.get(pk = pk)
         articles.delete()
-        return Response(status = 204)
+        return Response(status = status.HTTP_202_ACCEPTED)
     
             
             
